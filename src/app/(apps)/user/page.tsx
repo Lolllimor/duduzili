d0e4d997052fa23379a0457eee3f89c78336d26e
DeeBabaTech <dee1dayo4real@gmail.com>
2025-01-31 22:01:42 +0100

    changes

--- src/app/(apps)/user/page.tsx
+++ src/app/(apps)/user/page.tsx
@@ -1,24 +1,31 @@
 'use client';
 
-import { DataTable } from '@/lib/table-data';
-import { Skeleton } from '@/components/ui/skeleton';
-import { useCustomTable } from '@/lib/custom-data';
-import { communityAtom } from '@/lib/query-store';
-import { usePortal } from '@ibnlanre/portal';
-import GeneralLayout from '@/components/layout/generalLayout';
-import { EmptyState } from '@/components/settings/empty-state';
-import { useFetchCommunityListQuery } from '@/redux/features/communityApi';
-import { CommunityColumn } from '@/components/community/community-table-column';
-import { CommunityTableHeader } from '@/components/community/community-table-header';
-import { TableSkeleton } from '@/components/table-skeleton';
-import { SearchForm } from '@/components/search-comp';
-import { useState } from 'react';
+import { DataTable } from "@/lib/table-data";
+import { useCustomTable } from "@/lib/custom-data";
+import { userAtom } from "@/lib/query-store";
+import { Atom, usePortal } from "@ibnlanre/portal";
+import GeneralLayout from "@/components/layout/generalLayout";
+import { EmptyState } from "@/components/settings/empty-state";
+import { TableSkeleton } from "@/components/table-skeleton";
+import { SearchForm } from "@/components/search-comp";
+import { useState } from "react";
+import { useFetchUserListQuery } from "@/redux/features/userApi";
+import { UserColumn } from "@/components/community/user-table-column";
+import { UserCard } from "@/components/user/users-cards";
+import {
+  DropdownMenuTrigger,
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDown } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 function page() {
   const [queries, setQueries] = usePortal.atom(communityAtom);
 
   const [debounced, setDebounced] = useState<string>();
-  const { data, isLoading } = useFetchCommunityListQuery({
+  const { data, isLoading, isFetching } = useFetchUserListQuery({
     page: queries.page_index,
     search: debounced,
   });
@@ -26,30 +33,58 @@ function page() {
     tableData: data?.data.results,
     columns: CommunityColumn,
   });
+
   const handleSearch = (searchTerm: any) => {
     setDebounced(searchTerm);
   };
+
   return (
-    <GeneralLayout pageTitle="Communities" className="h-[calc(100vh-120px)]">
-      <div className="px-6 flex flex-col gap-6  pb-6  w-full">
-        <SearchForm placeholder="Search Community" onSearch={handleSearch} />
-        {isLoading ? (
-          <div className="flex flex-col bg-[#F9FAFB] rounded-2xl">
+    <GeneralLayout pageTitle='Users' className='h-[calc(100vh-120px)]'>
+      <UserCard />
+      <div className='px-6 flex flex-col gap-6  pb-6  w-full'>
+        <div className='flex items-center'>
+          <SearchForm placeholder='Search User' onSearch={handleSearch} />
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant='outline' className='ml-auto'>
+                All Genders <ChevronDown />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align='end'>
+              {table
+                .getAllColumns()
+                .filter((column) => column.getCanHide())
+                .map((column) => {
+                  return (
+                    <DropdownMenuCheckboxItem
+                      key={column.id}
+                      className='capitalize'
+                      checked={column.getIsVisible()}
+                      onCheckedChange={(value) =>
+                        column.toggleVisibility(!!value)
+                      }>
+                      {column.id}
+                    </DropdownMenuCheckboxItem>
+                  );
+                })}
+              {/* Doing this because the endpoint can't search either male or female */}
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+        {isLoading || isFetching ? (
+          <div className='flex flex-col bg-[#F9FAFB] rounded-2xl'>
             <TableSkeleton />
           </div>
         ) : data?.data.count ? (
-          <div className="border rounded-lg w-full overflow-x-auto  ">
+          <div className='border rounded-lg w-full overflow-x-auto  '>
             <DataTable
-              queryAtom={communityAtom}
+              queryAtom={userAtom}
               table={table}
               totalCount={data?.data.count}
             />
           </div>
         ) : (
-          <EmptyState
-            title="Communities"
-            paragraph="No community have been created"
-          />
+          <EmptyState title='Users' paragraph='No user found' />
         )}
       </div>
     </GeneralLayout>
